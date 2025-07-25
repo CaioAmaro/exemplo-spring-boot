3a36165cd01bc47eb4f2f5b2ea812704080498ec
Caio Amaro <54718957+CaioAmaro@users.noreply.github.com>
2025-07-25 10:30:10 -0300

    Initial commit

---
+++ src/main/java/com/amaro/exemplo_spring_boot/controller/HomeController.java
@@ -0,0 +1,14 @@
+package com.amaro.exemplo_spring_boot.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping
+    public String helloWorld(){
+        return "Hello, world!";
+    }
+
+}
---
+++ src/test/java/com/amaro/exemplo_spring_boot/ExemploSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amaro.exemplo_spring_boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExemploSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
